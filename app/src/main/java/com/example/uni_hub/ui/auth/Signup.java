0e31f82afbaa511651a3c303bae762e0eccfc5e2
macRos-Alzoubi <macRos.alzoubi@gmail.com>
2021-11-27 14:05:28 +0200

    signin/signup design and code

---
+++ app/src/main/java/com/example/uni_hub/ui/auth/Signup.java
@@ -0,0 +1,18 @@
+package com.example.uni_hub.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uni_hub.R;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        
+    }
+}
