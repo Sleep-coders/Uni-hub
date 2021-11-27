6c4d7623e77cbcf04ff337e98215b5f2b61588db
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-11-27 19:45:49 +0200

    add-carpool-driver

---
+++ app/src/main/java/com/example/uni_hub/ui/carpool/Carpool_Home.java
@@ -0,0 +1,67 @@
+package com.example.uni_hub.ui.carpool;
+import com.example.uni_hub.MainActivity;
+import com.example.uni_hub.databinding.ActivityMainBinding;
+import com.example.uni_hub.ui.carpool.driver.Driver_home;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.uni_hub.R;
+
+public class Carpool_Home extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private Button driver_button;
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carpool_home);
+
+        getSupportActionBar().hide();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+//                        finish();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        // Driver Role
+        driver_button = (Button) findViewById(R.id.driver_button);
+        driver_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                getDriverActivity();
+            }
+        });
+    }
+    // Driver Role
+    public void getDriverActivity(){
+        Intent driver_home = new Intent(this, Driver_home.class);
+        startActivity(driver_home);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
---
+++ app/src/main/java/com/example/uni_hub/ui/carpool/driver/Driver_Create_Ride.java
@@ -0,0 +1,40 @@
+package com.example.uni_hub.ui.carpool.driver;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.uni_hub.MainActivity;
+import com.example.uni_hub.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Driver_Create_Ride extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BottomNavigationView bottomNavigationView;
+
+        setContentView(R.layout.activity_driver_create_ride);
+
+        // Navbar Bottom
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
