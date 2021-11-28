3bb33785c6b20e3283f25532e14c3f63e8ed00d0
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-11-28 16:33:16 +0200

    day02-carpool-driver-activities-initial

---
+++ app/src/main/java/com/example/uni_hub/ui/carpool/driver/DriverAdapter.java
@@ -0,0 +1,64 @@
+package com.example.uni_hub.ui.carpool.driver;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.uni_hub.R;
+
+public class DriverAdapter extends RecyclerView.Adapter<DriverAdapter.DriverViewHolder> {
+
+    String data1[],data2[];
+    int images[];
+    Context context;
+
+    public DriverAdapter(Context ct, String s1[],String s2[], int img[]){
+        context= ct;
+        data1 = s1;
+        data2 = s2;
+        images = img;
+    }
+
+    @NonNull
+    @Override
+    public DriverViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.driver_raw,parent,false);
+        return new DriverViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DriverViewHolder holder, int position) {
+        holder.myText1.setText(data1[position]);
+        holder.myText2.setText(data2[position]);
+        holder.myImages.setImageResource(images[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public class DriverViewHolder extends RecyclerView.ViewHolder{
+
+        TextView myText1,myText2;
+        ImageView myImages;
+
+        public DriverViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myText1 = itemView.findViewById(R.id.driver_name_cards);
+            myText2 = itemView.findViewById(R.id.rout_path);
+            myImages = itemView.findViewById(R.id.imageView19);
+
+
+
+        }
+    }
+}
