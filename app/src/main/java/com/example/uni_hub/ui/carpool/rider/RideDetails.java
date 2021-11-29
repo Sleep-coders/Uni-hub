b6a7b1308dc17554140d6226f71b5809115b9d4c
hatemsallam <hatemsallam94@hotmail.com>
2021-11-29 18:34:44 +0200

    riderActivities

--- app/src/main/java/com/example/uni_hub/ui/carpool/rider/RideDetails.java
+++ app/src/main/java/com/example/uni_hub/ui/carpool/rider/RideDetails.java
@@ -2,8 +2,6 @@ package com.example.uni_hub.ui.carpool.rider;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,17 +11,13 @@ import com.example.uni_hub.MainActivity;
 import com.example.uni_hub.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Rider_home extends AppCompatActivity {
-
-    RecyclerView recyclerView;
-    String availableRides[], ridesDescription[], ridesDescription2[];
-    int images[] = {R.drawable.car1,R.drawable.car2,R.drawable.car3,R.drawable.car4};
+public class RideDetails extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rider_home);
-
+        setContentView(R.layout.activity_ride_details);
 
         getSupportActionBar().hide();
 
@@ -43,19 +37,5 @@ public class Rider_home extends AppCompatActivity {
                 return false;
             }
         });
-
-        availableRides = getResources().getStringArray(R.array.Available_Rides);
-        ridesDescription = getResources().getStringArray(R.array.rides_description);
-        ridesDescription2 = getResources().getStringArray(R.array.rides_description2);
-
-        recyclerView = findViewById(R.id.availaberides_recyclerview);
-
-        Rider_home_adapter rider_home_adapter = new Rider_home_adapter(this,availableRides,ridesDescription,ridesDescription2,images);
-
-        recyclerView.setAdapter(rider_home_adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-
-
-
     }
 }
