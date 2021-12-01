5dc478fec80a3314da8bd65167895dae90954ac4
DoaaDaban <doaadaban993@gmail.com>
2021-12-01 21:06:31 +0200

    add calendar

--- app/src/main/java/com/example/uni_hub/ui/utilities/Student_Utilities.java
+++ app/src/main/java/com/example/uni_hub/ui/utilities/Student_Utilities.java
@@ -2,7 +2,10 @@ package com.example.uni_hub.ui.utilities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.uni_hub.R;
 
@@ -12,5 +15,28 @@ public class Student_Utilities extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_utilities);
+
+
+        Intent intent=new Intent(this, Calendar_Activity.class);
+        Button btn= findViewById(R.id.btn_calender);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
+
+
+
+        Intent intent2=new Intent(this, Calendar_Activity.class);
+        Button btn2= findViewById(R.id.btn_pdf_scanner);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent2);
+            }
+        });
+
     }
 }
