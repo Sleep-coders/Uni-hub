51d762a8c1423ad6608bda281c7500f6df8ceede
Tareq Zeadeh <tareq.ha130@yahoo.com>
2021-12-01 19:57:32 +0200

    to mohammed

--- app/src/main/java/com/example/uni_hub/ui/carpool/driver/Available_Rides.java
+++ app/src/main/java/com/example/uni_hub/ui/carpool/driver/Available_Rides.java
@@ -8,7 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -28,6 +32,7 @@ import com.example.uni_hub.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.squareup.picasso.Picasso;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -46,6 +51,9 @@ public class Available_Rides extends AppCompatActivity {
     private TextView ride_description;
     private TextView ride_date;
 
+    Handler allRidesHandler;
+    Handler userIdHandler;
+
     private Ride owner_ride;
 
 
@@ -61,21 +69,31 @@ public class Available_Rides extends AppCompatActivity {
         setContentView(R.layout.activity_available_rides);
 
         getUserID();
-        getAllRides();
-        getOwnerRide();
-
-
-        Log.i("owner_ride>>>>>||||||||++++>>>>", "++++++>>>>>>>"+owner_ride);
-
-
-//        setDriverData();
-
 
         recycler_view_driver_post = findViewById(R.id.recycler_view_driver_post);
+        userIdHandler=new Handler(Looper.getMainLooper(),message -> {
+            userId = message.getData().getString("ID");
+            Log.i("USERID_InHandler++++>>>>", "++++++>>>>>>>" + userId);
+            getAllRides();
+            return false;
+        });
+        allRidesHandler = new Handler(Looper.getMainLooper(), message -> {
+            Log.i("ALL_RIDES>>>>>||||||||++++>>>>", "++++++>>>>>>>" + allRides);
+            getOwnerRide();
+            recycler_view_driver_post.setLayoutManager(new LinearLayoutManager(this));
+            recycler_view_driver_post.setAdapter(new DriverAdapter(allRides, userId, getApplicationContext()));
+            recycler_view_driver_post.getAdapter().notifyDataSetChanged();
+
+            return false;
+        });
+
+
+
+
+
+
 //        DriverAdapter driverAdapter = new DriverAdapter(allRides , userId);
-        recycler_view_driver_post.setLayoutManager(new LinearLayoutManager(this));
-        recycler_view_driver_post.setAdapter(new DriverAdapter(allRides , userId , getApplicationContext()));
-        recycler_view_driver_post.getAdapter().notifyDataSetChanged();
+
 
 
         // Navbar Bottom
@@ -103,71 +121,79 @@ public class Available_Rides extends AppCompatActivity {
     }
 
     private void getOwnerRide() {
-        for(Ride ride : allRides){
-            if(ride.getOwnerId().equals(userId)){
+        for (Ride ride : allRides) {
+            if (ride.getOwnerId().equals(userId)) {
                 owner_ride = ride;
-                Log.i("owner_ride>>>>>||||||||++++>>>>", "++++++>>>>>>>"+ride.getOwnerId() + " aanndd===>>"+userId );
+                Log.i("owner_ride>>>>>||||||||++++>>>>", "++++++>>>>>>>" + ride.getOwnerId() + " aanndd===>>" + userId);
             }
 
         }
     }
 
-    private void setDriverData() {
-        car_img = findViewById(R.id.car_image);
-        owner_name = findViewById(R.id.driver_name);
-        ride_departure_time = findViewById(R.id.departure_time_post);
-        cost = findViewById(R.id.cost_details);
-        available_seats = findViewById(R.id.available_seats);
-        car_Info = findViewById(R.id.car_Info);
-        ride_expires_at = findViewById(R.id.ride_expires_at_owner_ride);
-        ride_description = findViewById(R.id.ride_description);
-        ride_date = findViewById(R.id.ride_date);
-
-        Picasso.get().load(owner_ride.getCarImage()).into(car_img);
-        owner_name.setText(owner_ride.getOwnerName());
-        ride_departure_time.setText(owner_ride.getRideDepartureTime());
-        cost.setText(owner_ride.getCost().toString());
-        available_seats.setText(owner_ride.getAvailableSeats());
-        car_Info.setText(owner_ride.getCarInfo());
-        ride_expires_at.setText(owner_ride.getRideExpiresAt());
-        ride_description.setText(owner_ride.getRideDescription());
-        ride_date.setText(owner_ride.getRideDate());
-    }
+//    private void setDriverData() {
+//        car_img = findViewById(R.id.car_image);
+//        owner_name = findViewById(R.id.driver_name);
+//        ride_departure_time = findViewById(R.id.departure_time_post);
+//        cost = findViewById(R.id.cost_details);
+//        available_seats = findViewById(R.id.available_seats);
+//        car_Info = findViewById(R.id.car_Info);
+//        ride_expires_at = findViewById(R.id.ride_expires_at_owner_ride);
+//        ride_description = findViewById(R.id.ride_description);
+//        ride_date = findViewById(R.id.ride_date);
+//
+//        Picasso.get().load(owner_ride.getCarInfo()).into(car_img);
+//        owner_name.setText(owner_ride.getOwnerName());
+//        ride_departure_time.setText(owner_ride.getRideDepartureTime());
+//        cost.setText(owner_ride.getCost().toString());
+//        available_seats.setText(owner_ride.getAvailableSeats().toString());
+//        car_Info.setText(owner_ride.getCarImage());
+//        ride_expires_at.setText(owner_ride.getRideExpiresAt());
+//        ride_description.setText(owner_ride.getRideDescription());
+//        ride_date.setText(owner_ride.getRideDate());
+//    }
 
     public void getCreateRide() {
         Intent driver_create_ride = new Intent(this, Driver_Create_Ride.class);
         startActivity(driver_create_ride);
     }
 
-    public void getAllRides(){
+    public void getAllRides() {
         allRides = new ArrayList<>();
         Amplify.API.query(ModelQuery.list(Ride.class),
-                success->{
-            for (Ride ride : success.getData()){
-                allRides.add(ride);
-            }
+                success -> {
+                    for (Ride ride : success.getData()) {
+                        allRides.add(ride);
+                    }
+                    allRidesHandler.sendEmptyMessage(0);
+
 //                    Log.i("ALLRIDES++++>>>>", "++++++>>>>>>>"+success.getData());
-//                    Log.i("ALLRIDES++++>>>>", "++++++>>>>>>>"+allRides);
+//                    Log.i("ALLRIDES_STATEC++++>>>>", "++++++>>>>>>>"+allRides);
                 },
-                error->{runOnUiThread(()->{
-                    @SuppressLint("ShowToast") Toast toast = Toast.makeText(this, "No available rides for now, Try again later", Toast.LENGTH_LONG);
-                    toast.show();
+                error -> {
+                    runOnUiThread(() -> {
+                        @SuppressLint("ShowToast") Toast toast = Toast.makeText(this, "No available rides for now, Try again later", Toast.LENGTH_LONG);
+                        toast.show();
+                    });
+                    Log.i("getAllRides", "======> Error in getting all rides" + error.getMessage());
                 });
-                    Log.i("getAllRides", "======> Error in getting all rides"+ error.getMessage());
-        });
     }
 
     public void getUserID() {
+        Bundle bundle = new Bundle();
+        Message message = new Message();
         String email = Amplify.Auth.getCurrentUser().getUsername();
         Amplify.API.query(ModelQuery.list(AppUser.class, AppUser.USER_EMAIL.eq(email)),
                 success -> {
-
-                    for(AppUser user : success.getData().getItems()){
-                        userId = user.getId();
-//                        Log.i("USERID++++>>>>", "++++++>>>>>>>"+user.getId());
+                    String  id = "";
+                    for (AppUser user : success.getData().getItems()) {
+                        id = user.getId();
+                        Log.i("USERID++++>>>>", "++++++>>>>>>>" + user.getId());
                         break;
                     }
-//                    Log.i("USERID++++>>>>", "++++++>>>>>>>"+userId);
+                    bundle.putString("ID",id);
+                    message.setData(bundle);
+                    userIdHandler.sendMessage(message);
+//                    Log.i("USERID++++>>>>", "++++++>>>>>>>" + userId);
                 },
                 error -> {
                     Log.i("getUserID", "Error in getting user id");
